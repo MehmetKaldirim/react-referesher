d39222b14c51535c5e89ad8230afb90d44107028
Mehmet <mathdenizi@gmail.com>
2024-04-14 00:21:31 +0200

    split the component

---
+++ src/components/GoalList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./GoalList.css";
+const GoalList = () => {
+  return (
+    <ul className="goal-list">
+      <li>Learn Expres</li>
+      <li>Learn React</li>
+      <li>Learn Mongodb</li>
+    </ul>
+  );
+};
+
+export default GoalList;
